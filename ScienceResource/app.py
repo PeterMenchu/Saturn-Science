f124142aa0a84623a4b2dbb3f93bba67900bbb21
Peter Menchu <menchupter2@gmail.com>
2023-07-26 22:12:30 -0500

    Adding some initial content

--- ScienceResource/app.py
+++ ScienceResource/app.py
@@ -27,9 +27,31 @@ def contact():  # put application's code here
 def phys():  # put application's code here
     return render_template("/Physics/index.html")
 
-@app.route('/Physics/Kepler1.html')
+
+@app.route('/Astronomy/index.html')
+def astro():  # put application's code here
+    return render_template("/Astronomy/index.html")
+
+
+@app.route('/Astronomy/Kepler1.html')
 def kep1():  # put application's code here
-    return render_template("/Physics/Kepler1.html")
+    return render_template("/Astronomy/kepler1.html")
+
+
+@app.route('/CS/index.html')
+def CS():  # put application's code here
+    return render_template("/CS/index.html")
+
+
+@app.route('/Physics/vectorScalar.html')
+def vect():  # put application's code here
+    return render_template("/Physics/vectorScalar.html")
+
+
+@app.route('/study')
+def study():  # put application's code here
+    return render_template("study.html")
+
 
 if __name__ == '__main__':
     app.run()
